d435314ca27f6f7e46d53a53464e1748fc3d5da9
shahrukh <shoaibtayyab121@gmail.com>
2021-02-14 23:45:34 +0530

    created separate serializer

--- projects/serializers.py
+++ projects/serializers.py
@@ -2,13 +2,19 @@ from rest_framework import serializers
 from projects.models import Project, Task
 
 
-class ProjectSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Project
-        fields = ('id', 'name', 'description')
-        read_only_fields = ('id', )
-
 class TaskSerializer(serializers.ModelSerializer):
     class Meta:
         model = Task
         fields = '__all__'
+
+
+class ProjectSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Project
+        fields = ('id', 'name', 'description', 'created_at', 'tasks')
+        read_only_fields = ('id', 'tasks')
+
+
+class ProjectReadSerializer(ProjectSerializer):
+    tasks = TaskSerializer(many=True)
